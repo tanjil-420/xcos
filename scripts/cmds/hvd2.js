3dd6bf6eeba58f851e6a2cf1deb99132b3b9a24f
tanjil-420 <jodiidaseplz@gmail.com>
2025-09-13 03:29:21 +0600

    Update and rename hvdo.js to hvd2.js

--- scripts/cmds/hvd2.js
+++ scripts/cmds/hvd2.js
@@ -8,7 +8,7 @@ module.exports = {
 		role: 2,
 		shortDescription: "get hentai video",
 		longDescription: "it will send hentai  video",
-		category: "𝟭𝟴+",
+		category: "18+",
 		guide: "{p}{n}hvdo",
 	},
 
